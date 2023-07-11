0f222cc28043afa23d28a3b57d9c3c853bb8d982
Don G Madamannil <139125864+donLitmus7@users.noreply.github.com>
2023-07-11 17:00:32 +0530

    performance improvements

--- EmployeeWithLog/LoggerCreate.java
+++ EmployeeWithLog/LoggerCreate.java
@@ -1,12 +1,23 @@
-import java.io.BufferedWriter;
+import java.io.PrintWriter;
 import java.io.FileWriter;
 import java.time.format.DateTimeFormatter;  
 import java.time.LocalDateTime;    
 class LoggerCreate {
-	private static BufferedWriter f_writer;
+	private  static PrintWriter f_writer;
+	static{
+		try{
+		 f_writer =new PrintWriter(new FileWriter("Log.txt",true));
+	}
+	catch(Exception e)
+	{
+		;
+	}
+	
+	}
 	//create and open file
-	public static void CreateLog() throws Exception {
-		f_writer= new BufferedWriter(new FileWriter("Log.txt",true));
+	private static void CreateLog() throws Exception {
+		
+	
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
 		LocalDateTime now = LocalDateTime.now();  
 		f_writer.write("\n"+dtf.format(now)+"\t");
@@ -17,19 +28,19 @@ class LoggerCreate {
 		CreateLog();
 		f_writer.write("info\t"+message+"\t");
 		
-		f_writer.close();
+		f_writer.flush();
 	}
 	public static void logWarning(String message) throws Exception{
 		CreateLog();
 		f_writer.write("Warning\t"+message+"\t");
 		
-		f_writer.close();
+		f_writer.flush();
 	}
 	public static void logError(String message) throws Exception{
 		CreateLog();
 		f_writer.write("Error\t"+message+"\t");
 		
-		f_writer.close();
+		f_writer.flush();
 	}
 	
 	
